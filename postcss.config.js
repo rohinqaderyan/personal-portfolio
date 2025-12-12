89b2a94b0765f93dbc4ecf4e6a5b609a008875a4
Rohin Qaderyan <ahmad.qaderyan@pfizer.com>
2025-12-12 14:20:48 -0500

    Add version tag to PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
 /**
  * PostCSS Configuration
  * Tailwind CSS and Autoprefixer for cross-browser support
+ * @version 1.3.1
  */
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
